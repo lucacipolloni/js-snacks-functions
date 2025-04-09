e37be4e805896c8101a6a7c5732ab0138a8d65a9
lucacipolloni <lucacipolloni29@gmail.com>
2025-04-09 17:35:31 +0200

    iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,22 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
+const getInitials = (names) => {
+  const initials = [];
 
+  for (let i = 0; i < names.length; i++) {
+    const currentName = names[i];
+    const currentInitial = currentName[0];
+    initials.push(currentInitial);
+  }
+
+  return initials;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const namesInitials = getInitials(names);
+console.log(namesInitials);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]

c4cf23ed70e1d5c952b35758b3373f2a6eb31253
lucacipolloni <lucacipolloni29@gmail.com>
2025-04-09 18:11:49 +0200

    filtra-iniziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,19 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
+const filterByInitial = (list, filterChar) => {
+  const filteredList = [];
 
+  for (const element of list) {
+    const firstChar = element[0];
+    if (firstChar === filterChar) filteredList.push(element);
+  }
+
+  return filteredList;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(filterByInitial(names, "A"));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]

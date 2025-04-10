3c445ae3fe50d5c9dec80b7c5ddf5b89aef10644
lucacipolloni <lucacipolloni29@gmail.com>
2025-04-10 09:50:38 +0200

    conta-vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,30 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
+const countVowels = (text) => {
+  let vowelsNumber = 0;
 
+  for (let i = 0; i < text.length; i++) {
+    const currentChar = text[i][0];
+    console.log(currentChar);
+
+    if (
+      currentChar === "a" ||
+      currentChar === "e" ||
+      currentChar === "i" ||
+      currentChar === "o" ||
+      currentChar === "u"
+    ) {
+      vowelsNumber++;
+    }
+  }
+
+  return vowelsNumber;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(countVowels(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
